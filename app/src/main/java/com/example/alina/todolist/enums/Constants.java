0bbb09083fc8facb9d906404aa17e1f4204e3847
IvanGumeniuk <1996altair1996@gmail.com>
2017-11-30 20:10:08 +0200

    added checking pin

---
+++ app/src/main/java/com/example/alina/todolist/enums/Constants.java
@@ -0,0 +1,11 @@
+package com.example.alina.todolist.enums;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by 1996a on 30.11.2017.
+ */
+
+public final class Constants {
+    public static final long TimeForCheckPassword = TimeUnit.SECONDS.toMillis(5);
+}
